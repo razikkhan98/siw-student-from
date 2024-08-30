ccee02d1bac138cc6507bb0665c795692e296c32
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2024-08-30 16:48:57 +0530

    fix index file

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ const corsMethod = {
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
 };
 
+
 Dbconnection();
 app.use(bodyParser.json());
 app.use(cors(corsMethod));
 app.use("/api", UserRouter);
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || 8000, () => {
+
+
+app.listen(process.env.PORT ?? 5007, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
+
